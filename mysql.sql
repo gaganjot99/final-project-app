d307fd1e55ffaa64172496ccd7ca8bd91f0bf293
Gagan Kahal <73100470+gaganjot99@users.noreply.github.com>
2024-12-07 05:22:39 -0500

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -2,13 +2,13 @@ CREATE DATABASE IF NOT EXISTS employees;
 USE employees;
 
 CREATE TABLE IF NOT EXISTS employee(
-emp_id VARCHAR(20),
+emp_id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(20),
 last_name VARCHAR(20),
 primary_skill VARCHAR(20),
 location VARCHAR(20));
 
 INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
-INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('2','Alan','Williams','Empathy','alien');
 SELECT * FROM employee;
 
